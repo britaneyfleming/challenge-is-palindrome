de64806367fa72dae24f13aa5317f1f9d99e0375
Britaney Fleming <britaneyrfleming@gmail.com>
2022-11-10 09:51:17 -0600

    Final Commit

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,12 @@
 // Write your code below
+
+function isPalindrome(word){
+    let b = word.length - 1
+    for(let i = 0; i < word.length; i++){
+        if(word[i] != word[b--]){
+            return false
+        }
+    }
+    return true
+}
+console.log(isPalindrome('dad'))
